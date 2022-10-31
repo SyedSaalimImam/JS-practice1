4a6f3c3cf4cbb72c2071079f2d948673e1e322f9
SyedSaalimImam <syedsaalimimam@gmail.com>
2022-10-31 04:31:55 -0700

    first page

---
+++ app.js
@@ -0,0 +1,45 @@
+alert(" you have successfully submitted ")
+window.alert("heelooo");
+
+
+var abc="hello javascript"
+alert(abc)
+var a=("hello hoe are u?")
+alert(a)
+
+var b=10;
+alert(b);   
+
+
+
+
+
+
+var a=10
+var b=20
+var c=a+b
+var c=a+b+a*b/b
+
+alert(c)
+
+
+var v;
+
+v="skrrrrrrrrrrr";
+
+alert(v);
+
+
+var a=10
+var b="20"
+//concadinate
+var d= a+b
+alert(d)
+
+
+var l=10
+var r=20
+l++;
+r--
+console.log(r);
+console.log(l);
